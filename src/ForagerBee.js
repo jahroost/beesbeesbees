defe6efae93de640732d1f10c36d700cf5578c14
jahroost <claysmart@gmail.com>
2017-10-09 12:24:47 -0500

    Finished ForagerBee.js

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
 var ForagerBee = function() {
+  HoneyMakerBee.call(this);
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
+ForagerBee.prototype = Object.create(Grub.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
 
+ForagerBee.prototype.forage = function(value) {
+  this.treasureChest.push(value);
+}
